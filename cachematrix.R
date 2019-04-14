955d274fefa8a9c5e61fc6e55495c918b25ffa2c
jpmaillard <maillardjp@gmail.com>
2019-04-13 22:36:44 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,30 +1,35 @@
-## The two folling functions that will first create a matrice and then
-## calculate its inverse by caching the inverse in R memory.
-## We are asuming here (and therefore not checking it!) that
+## The two following functions that will first create a special Matrix
+## that can cache its inverse in memory.
+## We are assuming here (and therefore not checking it!) that
 ## the matrix entered will be square and that its determinant will
-## be different of 0.
+## be different of 0 (the two conditions needed for the matrix to be inversible).
 
-## This function "makeCacheMatrix" create and cache a matrix
+
+## This function "makeCacheMatrix" create and cache a matrix object "mymat"
+## object to which we will associate the attributes "theinv" that will be used
+## to cache its inverse. Multiple methods will be create to call
+## the matrix, its inverse (set, get, setinverse and getinverse). Finally a list
+## will be created and call to store all these methods, attributes in R memory.
 
 makeCacheMatrix <- function(mymat = matrix()) {
   theinv <- NULL
   set <- function(y) {
-    #
     mymat <<- y
     theinv <<- NULL
   }
   get <- function() mymat
   setinverse <- function(inverse) theinv <<- inverse
-  getinverse <- function() inv
+  getinverse <- function() theinv
   list(set = set, get = get,
        setinverse = setinverse,
        getinverse = getinverse)
 }
 
 
-## The following fuction "cacheSolve" will calculate the inverse
-## of the mymat Matrix previously used in the first function.
-## the inverse will be cache and therefore reusable.
+## The following function "cacheSolve" will calculate the inverse
+## of the "mymat" Matrix previously used in the first function.
+## If there is already a inverse been cached by previously calling these 2
+## functions.
 
 cacheSolve <- function(mymat, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -37,5 +42,5 @@ cacheSolve <- function(mymat, ...) {
   theinv <- solve(data, ...)
   mymat$setinverse(m)
   theinv
-  
+
 }
